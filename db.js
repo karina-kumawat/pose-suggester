aad087e66151d555b02b235c93a3107e5729203e
karina kumari <karinakkumawat@gmail.com>
2026-09-05 11:51:40 +0530

    Refactor MySQL pool configuration to use env variables
    Updated MySQL connection configuration to use environment variables.

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'pose123',
-  database: 'pose_suggester',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 module.exports = pool.promise();
